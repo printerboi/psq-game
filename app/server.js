386e6c0eda7f1ec11396a332bb620cbc4e06ecf4
Maximilian Krebs <maximilian-krebs@online.de>
2022-09-18 15:50:25 +0200

    Updated infrastructure to fit dockerization

--- app/server.js
+++ app/server.js
@@ -1,60 +1,31 @@
-const express = require('express')
+// server.js
+const { createServer } = require('http')
+const { parse } = require('url')
 const next = require('next')
-const http = require("http")
-const https = require("https")
-const fs = require("fs")
-    
+
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
+const hostname = 'localhost'
+const port = 3000
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
-let port = 3000;
 
-if (!dev){
-    port = 80;
-}
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      // Be sure to pass `true` as the second argument to `url.parse`.
+      // This tells it to parse the query portion of the URL.
+      const parsedUrl = parse(req.url, true);
+      const { pathname, query } = parsedUrl;
 
-let httpsOptions = {};
-if(!dev){
-    httpsOptions = {
-        key: fs.readFileSync("./.certificates/privkey.pem"),
-        cert: fs.readFileSync("./.certificates/cert.pem"),
-    };
-}
-    
-app.prepare()
-.then(() => {
-  const server = express();
-  if(!dev){
-    server.enable('trust proxy');
-  }
-
-  server.use("/uploads", express.static(__dirname + "/public/uploads"));
-    
-  server.all('*', (req, res) => {
-    if (!dev && !req.secure) {
-        return res.redirect("https://" + req.headers.host + req.url);
+      await handle(req, res, parsedUrl);
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err)
+      res.statusCode = 500
+      res.end('internal server error')
     }
-    return handle(req, res)
-  });
-    
-
-  const httpServer = http.createServer(server);
-
-  httpServer.listen(port, (err) => {
+  }).listen(port, (err) => {
     if (err) throw err
-    console.log(`> HTTP Ready on port ${port}`)
-  });
-
-  if(!dev){
-    const httpsServer = https.createServer(httpsOptions, server);
-
-    httpsServer.listen(443, (err) => {
-        if (err) throw err
-        console.log(`> HTTP Ready on port ${443}`)
-      });
-  }
-})
-.catch((ex) => {
-  console.error(ex.stack)
-  process.exit(1)
+    console.log(`> Ready on http://${hostname}:${port}`)
+  })
 })
